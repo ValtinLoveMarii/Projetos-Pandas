a9ee3eb03050ceef843d26bfe50bbdd0f7f43d7f
Valtin_Loves_Mari!!! <guilherme.toledo.m17@gmail.com>
2025-11-20 18:25:31 -0300

    commit 10
    Criação de funções e começo dos tratamentos e análise de dados no dataset de 2025

--- Analise_Decks_Cartas_ClashRoyale/src/features/criar_funcs.py
+++ Analise_Decks_Cartas_ClashRoyale/src/features/criar_funcs.py
@@ -12,3 +12,30 @@ def func_verificar_vencedor(valor):
 # FUNÇÃO PARA CRIAR UM LISTA, ONDE SE TERÁ O NÚMERO DE CARTAS POR LINHAS, NO FINAL TENDO OS DADOS COMPLETOS DE CADA LINHA (COM OS 2 DEKCS SENDO CONTADOS)
 def contar_cartas_por_linhas(valor):
     return valor['p1_1': 'p2_8'].to_list()
+
+
+# FUNC PARA TRANSFORMAR DICTS E LISTS EM SERIES E DEPOIS EM DATAFRAMES
+def trasnformar_series_em_df(valor, colunas=[]):
+    df = pd.Series(valor)
+    return pd.DataFrame(df, columns=[colunas]).sort_values(by=colunas, ascending=False)
+
+# RETORNAR O DECK VENCEDOR OU AMBOS OS DECK EM FORMA DE DICIONARIO
+
+''''
+    Função que retorna 2 possíveis resultados:
+        Um dicionário com todos os decks vencedores
+
+        Um dicionário com ambos os decks
+
+    Oque cada parâmetro faz:
+      tipo - 1 para utilizar a func que retorna apenas os decks vencedores e 2 para utilizar a func que retorna ambos os decks
+      df - recebe o dataframe com os decks para que se possa pegar os dados
+      axis - escolhe o axis, se irá pegar por coluna ou linha
+'''
+def retorna_deck_venc_ou_ambos(tipo, df, axis):
+    if tipo == 1:
+        df_intermedi = df.apply(func_verificar_vencedor, axis=axis)
+        return df_intermedi.to_dict()
+    elif tipo == 2:
+        df_intermedi = df.apply(contar_cartas_por_linhas, axis=axis)
+        return df_intermedi.to_dict()
--- Analise_Decks_Cartas_ClashRoyale/src/utils/criar_funcs_utils.py
+++ Analise_Decks_Cartas_ClashRoyale/src/utils/criar_funcs_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-# 1. Defina sua função (Correta)
+# CRIA UMA DATAFRAME JUNTANDO SERIES
 def criar_dataframe_juntando_series(valor1, valor2):
     return pd.concat([valor1, valor2], axis=1) 
+
